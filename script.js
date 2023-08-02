4db4c0cbc53c37a2e3f752e6c732d3f60b413201
rigs <rigorsyguatbsn@gmail.com>
2023-08-02 17:33:22 +0800

    Clean code

--- script.js
+++ script.js
@@ -3,41 +3,52 @@ let playerScore = 0,
 
 let choices = ['Rock', 'Paper', 'Scissors'];
 
+let firstLetterUpperCase = (str) => str[0].toUpperCase() + str.slice(1);
+
+startGame();
+
 function getComputerChoice(){
-    return choices[Math.floor(Math.random() * choices.length)].toLowerCase();
+    // double '~' means to truncate decimal places or a faster Math.floor()
+    return choices[~~(Math.random() * choices.length)].toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection){
     console.log(`You picked ${playerSelection}`);
     console.log(`Your opponent picked ${computerSelection}`);
-    if(playerSelection === computerSelection){
+    
+    if (playerSelection === computerSelection){
         console.log("Tied!")
-    }
-    else{
-        if(playerSelection === 'rock' && computerSelection === 'scissors'
+    } else {
+        if (playerSelection === 'rock' && computerSelection === 'scissors'
         || playerSelection === 'paper' && computerSelection === 'rock'
         || playerSelection === 'scissors' && computerSelection === 'paper'){
-            console.log(`You Win! ${playerSelection[0].toUpperCase() + playerSelection.slice(1)} beats ${computerSelection[0].toUpperCase() + computerSelection.slice(1)}`);
+            console.log(`You Win! ${firstLetterUpperCase(playerSelection)} beats ${firstLetterUpperCase(computerSelection)}`);
             playerScore++;
         } else {
-            console.log(`You Lose! ${computerSelection[0].toUpperCase() + computerSelection.slice(1)} beats ${playerSelection[0].toUpperCase() + playerSelection.slice(1)}`);
+            console.log(`You Lose! ${firstLetterUpperCase(computerSelection)} beats ${firstLetterUpperCase(playerSelection)}`);
             computerScore++;
         }
     }
+
     console.log(`${playerScore} - ${computerScore}`)
 }
 
-function gameStart(){
+function startGame(){
     let playerSelection = '';
+    
     for (let index = 0; index < 5; index++) {
+        // repeat until player picked a valid choice
         do {
             playerSelection = prompt("Pick: 'Rock', 'Paper' or 'Scissors'?").toLowerCase().trim();
-        } while (!choices.includes(`${playerSelection[0].toUpperCase() + playerSelection.slice(1)}`));
+        } while (!choices.includes(`${firstLetterUpperCase(playerSelection)}`));
+
         let computerSelection = getComputerChoice();
+
         playRound(playerSelection, computerSelection);
     }
+
     if (playerScore === computerScore) {
-        console.log('No winners! It\'s a tie!!!')
+        console.log('No winners! It\'s a tie!!!');
     } else {
         if (playerScore > computerScore) {
             console.log('Player Wins!')
@@ -46,5 +57,3 @@ function gameStart(){
         }
     }
 }
-
-gameStart();
